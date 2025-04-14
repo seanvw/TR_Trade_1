9b644d9ae013e2c7def5f7444a1b92368e247624
Sean Walsh <seanvw@gmail.com>
2025-04-14 11:01:14 +0200

    tidy reports

--- grep_summary.sh
+++ grep_summary.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 # This script is used to extract specific lines from the summary files generated 
- egrep '^Total Return on Investment \(ROI\) \[before tax and' *.txt | perl -ne '$foo=$.%2; print "$_" if $foo == 0' -
+ egrep '^Total Return on Investment \(ROI\) \[before tax and' my_sharedeal_reports/*.txt | perl -ne '$foo=$.%2; print "$_" if $foo == 0' -
 
